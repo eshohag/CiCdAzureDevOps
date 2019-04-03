cbd3460d91e7f6d421d3c76fc0df5a7c870e97ed
mdshohagmia <shohaghassan14@hotmail.com>
2019-04-03 15:51:09 +0600

    ChangeAppService

--- CiCdAzureDevOps/Controllers/ShohagController.cs
+++ CiCdAzureDevOps/Controllers/ShohagController.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 
 namespace CiCdAzureDevOps.Controllers
 {
@@ -10,6 +6,14 @@ namespace CiCdAzureDevOps.Controllers
     {
         // GET: Shohag
         public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult About()
+        {
+            return View();
+        }
+        public ActionResult Contact()
         {
             return View();
         }
