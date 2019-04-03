e41e8bae21d01c995a71bf851206c21e8ac2007c
mdshohagmia <shohaghassan14@hotmail.com>
2019-04-03 15:21:40 +0600

    UpdateShohagsApps

---
+++ CiCdAzureDevOps/Controllers/ShohagController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CiCdAzureDevOps.Controllers
+{
+    public class ShohagController : Controller
+    {
+        // GET: Shohag
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
